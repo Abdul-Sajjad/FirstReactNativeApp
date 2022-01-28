33292cc865f7d18c0c5c908b8e4aaa054a11a544
Abdul-Sajjad <abdulsajjad.shaikh@systemsltd.com>
2022-01-28 17:55:00 +0500

    dev branch new changes locally

--- app/src/main/java/com/example/gitworking/MainActivity.kt
+++ app/src/main/java/com/example/gitworking/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 // feature b branch working
 //New work on feature a
+
+// Changes in DEV locally and push this server
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
