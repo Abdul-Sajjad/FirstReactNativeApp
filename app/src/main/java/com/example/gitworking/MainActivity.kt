72ee5ce7d1daaad522758053db833b06e5eb4aeb
Abdul-Sajjad <78848510+Abdul-Sajjad@users.noreply.github.com>
2022-01-28 17:51:48 +0500

    dev branch changes commit

--- app/src/main/java/com/example/gitworking/MainActivity.kt
+++ app/src/main/java/com/example/gitworking/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 // feature b branch working
 //New work on feature a
+
+//CHANGES IN DEV BRANCH
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
