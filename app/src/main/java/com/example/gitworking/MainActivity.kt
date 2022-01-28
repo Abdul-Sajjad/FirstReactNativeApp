1a1e34d213990a2ba950c8161d453bd49d7df933
Abdul-Sajjad <abdulsajjad.shaikh@systemsltd.com>
2022-01-28 17:29:55 +0500

    feature_a branch new work

--- app/src/main/java/com/example/gitworking/MainActivity.kt
+++ app/src/main/java/com/example/gitworking/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.gitworking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//New work on feature a
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
